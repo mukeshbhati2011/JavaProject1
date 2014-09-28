b2e59f6986fd6aea64382e62951f41c2f0f18309
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-28 08:41:06 -0400

    First Commit by Dev 2: Changes done - New Java File : Dev2JavaFile1 - Edited existing file: DevelopJavaFile1

--- GitJavaProject1/src/DevelopJavaFile1.java
+++ GitJavaProject1/src/DevelopJavaFile1.java
@@ -5,4 +5,9 @@ public class DevelopJavaFile1
 	{
 		System.out.println("Develop Java File Line 1");
 	}
+	
+	public void dev2Method()
+	{
+		System.out.println("Message added by Dev 2 resource");
+	}
 }
---
+++ GitJavaProject1/src/Developer2/Dev2JavaFile1.java
@@ -0,0 +1,9 @@
+package Developer2;
+
+public class Dev2JavaFile1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Lines added by Dev 2: Dev2JavaFile1: Line 1");
+	}
+}
